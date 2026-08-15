649b92685545aae8818f8392ea2e2effc54b8882
7Bpencil <Edward.Ekb@yandex.ru>
2026-08-15 07:18:34 +0500

    load profiles from server, equip weapon in hands, remove obsolete stuff

--- Client/Patches.cs
+++ Client/Patches.cs
@@ -24,48 +24,5 @@ using UnityEngine.SceneManagement;
 
 namespace SevenBoldPencil.TargetMannequins
 {
-    public struct StashItemModelSpawnPoint_Proxy(StashItemModelSpawnPoint instance)
-    {
-        private readonly StashItemModelSpawnPoint __instance = instance;
 
-        private static TypedFieldInfo<StashItemModelSpawnPoint, List<StashItemModel>> __items = new("_items");
-
-        public List<StashItemModel> _items { get { return __items.Get(__instance); } set { __items.Set(__instance, value); } }
-    }
-
-	public struct StashItemModel_Proxy(StashItemModel instance)
-	{
-        private readonly StashItemModel __instance = instance;
-
-        private static TypedFieldInfo<StashItemModel, GameObject> __prefab = new("_prefab");
-        private static TypedFieldInfo<StashItemModel, Transform> __rotationPivot = new("_rotationPivot");
-
-        public GameObject _prefab { get { return __prefab.Get(__instance); } set { __prefab.Set(__instance, value); } }
-        public Transform _rotationPivot { get { return __rotationPivot.Get(__instance); } set { __rotationPivot.Set(__instance, value); } }
-	}
-
-	public class Patch_HideoutAreaStashController_UpdateStash : ModulePatch
-	{
-        protected override MethodBase GetTargetMethod()
-        {
-            return AccessTools.Method(typeof(HideoutAreaStashController), nameof(HideoutAreaStashController.UpdateStash));
-        }
-
-        [PatchPostfix]
-        public static void Postfix(ref Task __result, HideoutAreaStashController __instance, bool clearCache = false)
-		{
-			if (__instance._areaType != EAreaType.EquipmentPresetsStand)
-			{
-				return;
-			}
-	        __result = __result.ContinueWith(t =>
-	        {
-	            if (t.IsCompletedSuccessfully)
-	            {
-					Plugin.Instance.PatchMannequins(__instance);
-	            }
-	            return t;
-	        }).Unwrap();
-		}
-	}
 }
--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -43,25 +43,16 @@ using UnityEngine;
 
 namespace SevenBoldPencil.TargetMannequins
 {
-    [BepInPlugin("7Bpencil.TargetMannequins", "7Bpencil.TargetMannequins", "1.0.0")]
+    [BepInPlugin("7Bpencil.TargetMannequins", "7Bpencil.TargetMannequins", "0.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static Plugin Instance;
 		public ManualLogSource LoggerInstance;
 
-		// TODO make it a setting
-		public float Health_Head = 35;
-		public float Health_Chest = 85;
-		public float Health_Stomach = 70;
-		public float Health_Arm = 60;
-		public float Health_Leg = 65;
-
         private void Awake()
         {
             Instance = this;
 			LoggerInstance = Logger;
-
-			new Patch_HideoutAreaStashController_UpdateStash().Enable();
         }
 
 		public void Update()
@@ -70,115 +61,6 @@ namespace SevenBoldPencil.TargetMannequins
 			{
 				SpawnBot();
 			}
-			if (Input.GetKeyDown(KeyCode.F13))
-			{
-				DumpAllPlayersProfiles();
-			}
-		}
-
-		public void DumpAllPlayersProfiles()
-		{
-			var gameWorld = Singleton<GameWorld>.Instance;
-			var players = gameWorld.GetField<GameWorld, Dictionary<string, Player>>("_allPlayersEverExisted");
-			foreach (var (playerName, player) in players)
-			{
-				var profileDescriptor = new ProfileDescriptor(player.Profile, FullySearchedSearchController.Instance);
-				var prettyJson = profileDescriptor.ToPrettyJson(Array.Empty<JsonConverter>());
-				File.WriteAllText($"dumped_profiles/{playerName}.json", prettyJson);
-			}
-		}
-
-		public static ProfileDescriptor LoadDumpedProfile(string id)
-		{
-			var text = File.ReadAllText($"dumped_profiles/{id}.json");
-			return text.ParseJsonTo<ProfileDescriptor>(Array.Empty<JsonConverter>());
-		}
-
-		public static Profile.HealthInfo.ValueInfo GetHealthValueInfo(float currentValue, float minValue, float maxValue)
-		{
-			return new()
-			{
-				Current = currentValue,
-				Minimum = minValue,
-				Maximum = maxValue,
-			};
-		}
-
-		public static Profile.HealthInfo.ValueInfo GetHealthValueInfo(float maxValue)
-		{
-			return GetHealthValueInfo(maxValue, 0, maxValue);
-		}
-
-		public static Profile.HealthInfo.BodyPartInfo GetBodyPartInfo(float maxHealthValue)
-		{
-			return new()
-			{
-				Health = GetHealthValueInfo(maxHealthValue),
-			};
-		}
-
-		public static InventoryDescriptor MakeDefaultInventory()
-		{
-			// TODO copy equipment from mannequin (not entire inventory)
-		    var equipment = MongoID.Generate(true);
-			return new()
-			{
-				_items =
-				[
-					new()
-					{
-						_id = equipment,
-						_tpl = "55d7217a4bdc2d86028b456d",
-					},
-				],
-				_equipmentId = equipment,
-				_hideoutAreaStashesIds = new(),
-			};
-		}
-
-		public ProfileDescriptor GenerateProfile()
-		{
-			return new()
-			{
-				Id = MongoID.Generate(true),
-				AccountId = "0",
-				Info = new()
-				{
-					Nickname = "Mannequin",
-					Side = EPlayerSide.Savage,
-					GameVersion = "",
-					Type = EProfileType.Eft,
-					Level = 1,
-				},
-				Customization = new()
-				{
-					// TODO different voice
-				    { EBodyModelPart.Head, "6644d2da35d958070c02642c" },
-				    { EBodyModelPart.Body, "6644d2ffd85107e63500a61c" },
-				    { EBodyModelPart.Feet, "6644d32235d958070c02642e" },
-				    { EBodyModelPart.Hands, "5cc2e68f14c02e28b47de290" },
-				    { EBodyModelPart.Voice, "5fc613c80b735e7b024c76e2" },
-				},
-				Health = new()
-				{
-					BodyParts = new()
-					{
-						{ EBodyPart.Head, GetBodyPartInfo(Health_Head) },
-						{ EBodyPart.Chest, GetBodyPartInfo(Health_Chest) },
-						{ EBodyPart.Stomach, GetBodyPartInfo(Health_Stomach) },
-						{ EBodyPart.LeftArm, GetBodyPartInfo(Health_Arm) },
-						{ EBodyPart.RightArm, GetBodyPartInfo(Health_Arm) },
-						{ EBodyPart.LeftLeg, GetBodyPartInfo(Health_Leg) },
-						{ EBodyPart.RightLeg, GetBodyPartInfo(Health_Leg) },
-					},
-					Energy = GetHealthValueInfo(100),
-					Hydration = GetHealthValueInfo(100),
-					Temperature = GetHealthValueInfo(36.6f, 28, 40),
-					Poison = GetHealthValueInfo(0, 0, 100),
-					UpdateTime = 0,
-				},
-				Inventory = MakeDefaultInventory(),
-			};
 		}
 
 		public async Task SpawnBot()
@@ -195,22 +77,13 @@ namespace SevenBoldPencil.TargetMannequins
 			var hideoutGame = hideoutController.task_0.Result.Value;
 			var hideoutGameWorld = hideoutGame.GameWorld;
 
-			// var localPlayer = hideoutGame.LocalPlayer;
-			// var botPlayerProfile = localPlayer.Profile.Clone();
-
-			// var botPlayerProfile = await ProfileStorage.GenerateProfile(EPlayerSide.Savage, DebugBotProfileChooser.Rifle);
-
-			var profileDescriptor = GenerateProfile();
-			var botPlayerProfile = new Profile(profileDescriptor);
-
-			// var profileDescriptor = LoadDumpedProfile("6a7f8f1aaf5c0f120081d6d9");
-			// var botPlayerProfile = new Profile(profileDescriptor);
-
+			var session = tarkovApplication.Session;
+			var profilesRequest = new List<CountTypeBotWave>()
 			{
-				// var profileDescriptor = new ProfileDescriptor(botPlayerProfile, FullySearchedSearchController.Instance);
-				// var prettyJson = profileDescriptor.ToPrettyJson(Array.Empty<JsonConverter>());
-				// File.WriteAllText($"bot.json", prettyJson);
-			}
+				new(1, WildSpawnType.bossZryachiy, BotDifficulty.normal),
+			};
+			var profiles = await session.LoadBots(profilesRequest);
+			var botPlayerProfile = profiles[0];
 
 			await Singleton<ObjectsFactory>.Instance.LoadBundlesAndCreatePools
 			(
@@ -258,43 +131,13 @@ namespace SevenBoldPencil.TargetMannequins
 			var playerCulling = botPlayer.GetField<LocalPlayer, OfflinePlayerCulling>("botPlayerCulling");
 			playerCulling.SetMode(BasePlayerCulling.EMode.Visible);
 
-			// TODO make them hold weapon if there is one
-			// botPlayer.ProceduralWeaponAnimation.Mask = EProceduralAnimationMask.Aiming;
+			// take weapon in hands
+			botPlayer.SetSlotItem(EquipmentSlot.FirstPrimaryWeapon, (_) => {});
 
 			}
 			catch (Exception e)
 			{
 				Logger.LogError(e);
-			}
-		}
-
-		public void PatchMannequins(HideoutAreaStashController __instance)
-		{
-			// TODO this one is called every time mannequin equipment is changed,
-			// so track when shit is needed and when not,
-			// TODO we probably should make our own mannequins and dont touch bsg ones
-			// TODO setup all body part colliders correctly
-
-			// TODO get equipment from mannequin
-
-			var spawnPointName = "Stand1";
-			var spawnPoint = __instance._stashObjectsSpawnPoints[spawnPointName];
-			var _spawnPoint = new StashItemModelSpawnPoint_Proxy(spawnPoint);
-			var items = _spawnPoint._items;
-			if (items.Count == 0)
-			{
-				Logger.LogWarning($"{spawnPointName} no items");
-				return;
-			}
-
-			var item = items[0];
-			var _item = new StashItemModel_Proxy(item);
-			var prefab = _item._prefab;
-			var rotationPivot = _item._rotationPivot;
-			if (!prefab.TryGetComponent<PlayerBody>(out var playerBody))
-			{
-				Logger.LogWarning($"{spawnPointName} no PlayerBody");
-				return;
 			}
 		}
     }
