45be869d7944e1399f51df5966efbc8e572db393
7Bpencil <Edward.Ekb@yandex.ru>
2026-08-15 05:11:34 +0500

    spawn bot with any profile

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -9,18 +9,26 @@ using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using Comfort.Common;
+using Diz.Jobs;
 using DG.Tweening;
 using EFT;
+using EFT.Animations;
 using EFT.Ballistics;
+using EFT.Development;
 using EFT.Interactive;
 using EFT.InventoryLogic;
 using EFT.Hideout;
 using Newtonsoft.Json;
+using HarmonyLib;
 using SevenBoldPencil.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 // TargetMannequins
@@ -35,75 +43,9 @@ using UnityEngine;
 
 namespace SevenBoldPencil.TargetMannequins
 {
-	public readonly record struct MannequinData
-	(
-		string SpawnPointName,
-		Vector3 LocalPosition,
-		float LocalEulerAnglesY
-	);
-
-	public class MannequinPlayer
-	{
-		public readonly Transform RotationPivot;
-		public readonly Dictionary<EBodyPart, float> Health;
-		public bool IsAlive;
-
-		public MannequinPlayer(Transform rotationPivot)
-		{
-			RotationPivot = rotationPivot;
-			Health = new(7);
-			ResetHealth();
-		}
-
-		public void ResetHealth()
-		{
-			Health[EBodyPart.Head] = Plugin.Instance.Health_Head;
-			Health[EBodyPart.Chest] = Plugin.Instance.Health_Chest;
-			Health[EBodyPart.Stomach] = Plugin.Instance.Health_Stomach;
-			Health[EBodyPart.LeftArm] = Plugin.Instance.Health_Arm;
-			Health[EBodyPart.RightArm] = Plugin.Instance.Health_Arm;
-			Health[EBodyPart.LeftLeg] = Plugin.Instance.Health_Leg;
-			Health[EBodyPart.RightLeg] = Plugin.Instance.Health_Leg;
-			IsAlive = true;
-		}
-
-		public float GetTotalHealth()
-		{
-			var sum = 0f;
-			foreach (var bodyPartHealth in Health.Values)
-			{
-				sum += bodyPartHealth;
-			}
-			return sum;
-		}
-	}
-
     [BepInPlugin("7Bpencil.TargetMannequins", "7Bpencil.TargetMannequins", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-		// TODO scale them down a little to match player height
-		public static readonly MannequinData[] MannequinsData =
-		[
-			new
-			(
-				SpawnPointName: "Stand1",
-				LocalPosition: new(-9.6436f, 0.9881f, 11.0927f),
-				LocalEulerAnglesY: 180
-			),
-			new
-			(
-				SpawnPointName: "Stand2",
-				LocalPosition: new(-11.4582f, 0.9883f, 3.3036f),
-				LocalEulerAnglesY: 135
-			),
-			new
-			(
-				SpawnPointName: "Stand3",
-				LocalPosition: new(-8.3382f, 0.9881f, -3.5891f),
-				LocalEulerAnglesY: 90
-			),
-		];
-
         public static Plugin Instance;
 		public ManualLogSource LoggerInstance;
 
@@ -122,6 +64,210 @@ namespace SevenBoldPencil.TargetMannequins
 			new Patch_HideoutAreaStashController_UpdateStash().Enable();
         }
 
+		public void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.F3))
+			{
+				SpawnBot();
+			}
+			if (Input.GetKeyDown(KeyCode.F13))
+			{
+				DumpAllPlayersProfiles();
+			}
+		}
+
+		public void DumpAllPlayersProfiles()
+		{
+			var gameWorld = Singleton<GameWorld>.Instance;
+			var players = gameWorld.GetField<GameWorld, Dictionary<string, Player>>("_allPlayersEverExisted");
+			foreach (var (playerName, player) in players)
+			{
+				var profileDescriptor = new ProfileDescriptor(player.Profile, FullySearchedSearchController.Instance);
+				var prettyJson = profileDescriptor.ToPrettyJson(Array.Empty<JsonConverter>());
+				File.WriteAllText($"dumped_profiles/{playerName}.json", prettyJson);
+			}
+		}
+
+		public static ProfileDescriptor LoadDumpedProfile(string id)
+		{
+			var text = File.ReadAllText($"dumped_profiles/{id}.json");
+			return text.ParseJsonTo<ProfileDescriptor>(Array.Empty<JsonConverter>());
+		}
+
+		public static Profile.HealthInfo.ValueInfo GetHealthValueInfo(float currentValue, float minValue, float maxValue)
+		{
+			return new()
+			{
+				Current = currentValue,
+				Minimum = minValue,
+				Maximum = maxValue,
+			};
+		}
+
+		public static Profile.HealthInfo.ValueInfo GetHealthValueInfo(float maxValue)
+		{
+			return GetHealthValueInfo(maxValue, 0, maxValue);
+		}
+
+		public static Profile.HealthInfo.BodyPartInfo GetBodyPartInfo(float maxHealthValue)
+		{
+			return new()
+			{
+				Health = GetHealthValueInfo(maxHealthValue),
+			};
+		}
+
+		public static InventoryDescriptor MakeDefaultInventory()
+		{
+			// TODO copy equipment from mannequin (not entire inventory)
+		    var equipment = MongoID.Generate(true);
+			return new()
+			{
+				_items =
+				[
+					new()
+					{
+						_id = equipment,
+						_tpl = "55d7217a4bdc2d86028b456d",
+					},
+				],
+				_equipmentId = equipment,
+				_hideoutAreaStashesIds = new(),
+			};
+		}
+
+		public ProfileDescriptor GenerateProfile()
+		{
+			return new()
+			{
+				Id = MongoID.Generate(true),
+				AccountId = "0",
+				Info = new()
+				{
+					Nickname = "Mannequin",
+					Side = EPlayerSide.Savage,
+					GameVersion = "",
+					Type = EProfileType.Eft,
+					Level = 1,
+				},
+				Customization = new()
+				{
+					// TODO different voice
+				    { EBodyModelPart.Head, "6644d2da35d958070c02642c" },
+				    { EBodyModelPart.Body, "6644d2ffd85107e63500a61c" },
+				    { EBodyModelPart.Feet, "6644d32235d958070c02642e" },
+				    { EBodyModelPart.Hands, "5cc2e68f14c02e28b47de290" },
+				    { EBodyModelPart.Voice, "5fc613c80b735e7b024c76e2" },
+				},
+				Health = new()
+				{
+					BodyParts = new()
+					{
+						{ EBodyPart.Head, GetBodyPartInfo(Health_Head) },
+						{ EBodyPart.Chest, GetBodyPartInfo(Health_Chest) },
+						{ EBodyPart.Stomach, GetBodyPartInfo(Health_Stomach) },
+						{ EBodyPart.LeftArm, GetBodyPartInfo(Health_Arm) },
+						{ EBodyPart.RightArm, GetBodyPartInfo(Health_Arm) },
+						{ EBodyPart.LeftLeg, GetBodyPartInfo(Health_Leg) },
+						{ EBodyPart.RightLeg, GetBodyPartInfo(Health_Leg) },
+					},
+					Energy = GetHealthValueInfo(100),
+					Hydration = GetHealthValueInfo(100),
+					Temperature = GetHealthValueInfo(36.6f, 28, 40),
+					Poison = GetHealthValueInfo(0, 0, 100),
+					UpdateTime = 0,
+				},
+				Inventory = MakeDefaultInventory(),
+			};
+		}
+
+		public async Task SpawnBot()
+		{
+			try
+			{
+
+    		if (!TarkovApplication.Exist(out var tarkovApplication))
+            {
+                return;
+            }
+
+			var hideoutController = tarkovApplication.HideoutControllerAccess;
+			var hideoutGame = hideoutController.task_0.Result.Value;
+			var hideoutGameWorld = hideoutGame.GameWorld;
+
+			// var localPlayer = hideoutGame.LocalPlayer;
+			// var botPlayerProfile = localPlayer.Profile.Clone();
+
+			// var botPlayerProfile = await ProfileStorage.GenerateProfile(EPlayerSide.Savage, DebugBotProfileChooser.Rifle);
+
+			var profileDescriptor = GenerateProfile();
+			var botPlayerProfile = new Profile(profileDescriptor);
+
+			// var profileDescriptor = LoadDumpedProfile("6a7f8f1aaf5c0f120081d6d9");
+			// var botPlayerProfile = new Profile(profileDescriptor);
+
+			{
+				// var profileDescriptor = new ProfileDescriptor(botPlayerProfile, FullySearchedSearchController.Instance);
+				// var prettyJson = profileDescriptor.ToPrettyJson(Array.Empty<JsonConverter>());
+				// File.WriteAllText($"bot.json", prettyJson);
+			}
+
+			await Singleton<ObjectsFactory>.Instance.LoadBundlesAndCreatePools
+			(
+				ObjectsFactory.PoolsCategory.Raid,
+				ObjectsFactory.AssemblyType.Local,
+				botPlayerProfile.GetAllPrefabPaths(true).ToArray<ResourceKey>(),
+				JobYieldPriority.General,
+				null,
+				ObjectsFactory.DefaultCancellationToken
+			);
+
+			var botPlayerId = hideoutGame.NextPlayerId();
+			var position = new Vector3(-3f, 0.01f, 19);
+			var rotation = Quaternion.Euler(0, 180, 0);
+
+			var botPlayer = await LocalPlayer.Create
+			(
+				gameWorld: hideoutGameWorld,
+				playerId: botPlayerId,
+				position: position,
+				rotation: rotation,
+				layerName: "Player",
+				prefix: "",
+				pointOfView: EPointOfView.ThirdPerson,
+				profile: botPlayerProfile,
+				aiControl: true,
+				updateQueue: hideoutGame.UpdateQueue,
+				armsUpdateMode: Player.EUpdateMode.Auto,
+				bodyUpdateMode: Player.EUpdateMode.Auto,
+				characterControllerMode: AppEnvironment.Config.CharacterController.BotPlayerMode,
+				getSensitivity: new Func<float>(LocalGame.CG_Class1642.CG_Class1642.method_4),
+				getAimingSensitivity: new Func<float>(LocalGame.CG_Class1642.CG_Class1642.method_5),
+				statisticsManager: new DumbStatisticsManager(),
+				filter: ThirdPersonCustomizationFilter.Default,
+				session: null,
+				localMode: ELocalMode.TRAINING,
+				isYourPlayer: false,
+				isBot: true
+			);
+
+			// TODO for some reason clothes skinned mesh renderers have enabled forceRenderingOff,
+			// I guess culling component thinks that they are not in camera view because
+			// something is not initalized properly, so for now just force rendering on
+
+			var playerCulling = botPlayer.GetField<LocalPlayer, OfflinePlayerCulling>("botPlayerCulling");
+			playerCulling.SetMode(BasePlayerCulling.EMode.Visible);
+
+			// TODO make them hold weapon if there is one
+			// botPlayer.ProceduralWeaponAnimation.Mask = EProceduralAnimationMask.Aiming;
+
+			}
+			catch (Exception e)
+			{
+				Logger.LogError(e);
+			}
+		}
+
 		public void PatchMannequins(HideoutAreaStashController __instance)
 		{
 			// TODO this one is called every time mannequin equipment is changed,
@@ -129,158 +275,41 @@ namespace SevenBoldPencil.TargetMannequins
 			// TODO we probably should make our own mannequins and dont touch bsg ones
 			// TODO setup all body part colliders correctly
 
-			foreach (var mannequinData in MannequinsData)
+			// TODO get equipment from mannequin
+
+			var spawnPointName = "Stand1";
+			var spawnPoint = __instance._stashObjectsSpawnPoints[spawnPointName];
+			var _spawnPoint = new StashItemModelSpawnPoint_Proxy(spawnPoint);
+			var items = _spawnPoint._items;
+			if (items.Count == 0)
 			{
-				var spawnPoint = __instance._stashObjectsSpawnPoints[mannequinData.SpawnPointName];
-				var _spawnPoint = new StashItemModelSpawnPoint_Proxy(spawnPoint);
-				var items = _spawnPoint._items;
-				if (items.Count == 0)
-				{
-					Logger.LogWarning($"{mannequinData.SpawnPointName} no items");
-					continue;
-				}
+				Logger.LogWarning($"{spawnPointName} no items");
+				return;
+			}
 
-				var item = items[0];
-				var _item = new StashItemModel_Proxy(item);
-				var prefab = _item._prefab;
-				var rotationPivot = _item._rotationPivot;
-				if (!prefab.TryGetComponent<PlayerBody>(out var playerBody))
-				{
-					Logger.LogWarning($"{mannequinData.SpawnPointName} no PlayerBody");
-					continue;
-				}
-
-				rotationPivot.localPosition = mannequinData.LocalPosition;
-				rotationPivot.localEulerAngles = new(0, mannequinData.LocalEulerAnglesY, 0);
-
-				var player = new MannequinPlayer(prefab.transform);
-
-				var playerBones = playerBody.PlayerBones;
-				var colliders = playerBones.BodyPartColliders;
-				foreach (var collider in colliders)
-				{
-					collider.gameObject.layer = LayersMaskController.HitColliderLayer;
-					// collider.InitColliderSettings(); // TODO setup correct settings before this
-					collider.playerBridge = new MannequinPlayerBridge(player);
-				}
-
-				Logger.LogWarning($"{mannequinData.SpawnPointName} done!");
+			var item = items[0];
+			var _item = new StashItemModel_Proxy(item);
+			var prefab = _item._prefab;
+			var rotationPivot = _item._rotationPivot;
+			if (!prefab.TryGetComponent<PlayerBody>(out var playerBody))
+			{
+				Logger.LogWarning($"{spawnPointName} no PlayerBody");
+				return;
 			}
 		}
     }
 
-	public class MannequinPlayerBridge : BodyPartCollider.IObserverToPlayerBridge
-	{
-		public readonly MannequinPlayer MannequinPlayer;
+    public static class R
+    {
+        public static V GetField<T, V>(this T instance, string fieldName)
+        {
+            return (V)AccessTools.Field(typeof(T), fieldName).GetValue(instance);
+        }
 
-		public MannequinPlayerBridge(MannequinPlayer mannequinPlayer)
-		{
-			MannequinPlayer = mannequinPlayer;
-		}
-
-		public IPlayer iPlayer
-		{
-			get
-			{
-				Plugin.Instance.LoggerInstance.LogWarning("iPlayer");
-				return null;
-			}
-		}
-
-		public float WorldTime
-		{
-			get
-			{
-				Plugin.Instance.LoggerInstance.LogWarning("WorldTime");
-				return 0;
-			}
-		}
-
-		public bool UsingSimplifiedSkeleton
-		{
-			get
-			{
-				Plugin.Instance.LoggerInstance.LogWarning("UsingSimplifiedSkeleton");
-				return false;
-			}
-		}
-
-		public PlayerHitInfo ApplyShot(DamageInfo damageInfo, EBodyPart bodyPart, EBodyPartColliderType bodyPartCollider, EArmorPlateCollider armorPlateCollider, ShotId shotId)
-		{
-			// TODO finish damage spread and commit that implementing damage+armor system ourselves is a bad idea,
-			// just try to spawn bot, then make custom one
-			if (MannequinPlayer.IsAlive)
-			{
-				var health = MannequinPlayer.Health;
-
-				Plugin.Instance.LoggerInstance.LogWarning($"ApplyShot: part: {bodyPart}, health: {health[bodyPart]}, total: {MannequinPlayer.GetTotalHealth()}, damage: {damageInfo.Damage}");
-
-				if (health[bodyPart] > 0)
-				{
-					health[bodyPart] = Math.Max(health[bodyPart] - damageInfo.Damage, 0);
-					if (health[bodyPart] == 0)
-					{
-						if (bodyPart == EBodyPart.Head || bodyPart == EBodyPart.Chest)
-						{
-							Kill();
-						}
-						// TODO spread damage over non destroyed body parts
-					}
-				}
-				else
-				{
-					// TODO spread damage over non destroyed body parts
-				}
-			}
-			return null;
-		}
-
-		private void Kill()
-		{
-			MannequinPlayer.IsAlive = false;
-
-			// TODO make animations more sexy
-			var sequence = DOTween.Sequence();
-			sequence.Append(MannequinPlayer.RotationPivot.DOLocalRotate(new(-90, 0, 0), 0.5f));
-			sequence.AppendInterval(3);
-			sequence.Append(MannequinPlayer.RotationPivot.DOLocalRotate(new(0, 0, 0), 0.5f));
-			sequence.AppendCallback(() =>
-			{
-				MannequinPlayer.ResetHealth();
-			});
-		}
-
-
-		public void ApplyDamageInfo(DamageInfo damageInfo, EBodyPart bodyPartType, EBodyPartColliderType bodyPartCollider, float absorbed)
-		{
-			Plugin.Instance.LoggerInstance.LogWarning("ApplyDamageInfo");
-		}
-
-		public bool TryGetArmorResistData(BodyPartCollider bodyPart, float penetrationPower, out ArmorResistanceData armorResistanceData)
-		{
-			Plugin.Instance.LoggerInstance.LogWarning("TryGetArmorResistData");
-			armorResistanceData = default;
-			return false;
-		}
-
-		public bool SetShotStatus(BodyPartCollider bodypart, Shot shot, Vector3 hitpoint, Vector3 shotNormal, Vector3 shotDirection)
-		{
-			Plugin.Instance.LoggerInstance.LogWarning("SetShotStatus");
-			// either deflected by armor, or set
-			return false;
-		}
-
-		public bool CheckArmorHitByDirection(BodyPartCollider bodypart, Vector3 hitpoint, Vector3 shotNormal, Vector3 shotDirection)
-		{
-			Plugin.Instance.LoggerInstance.LogWarning("CheckArmorHitByDirection");
-			return false;
-		}
-
-		public bool IsShotDeflectedByHeavyArmor(EBodyPartColliderType colliderType, EArmorPlateCollider armorPlateCollider, int shotSeed)
-		{
-			Plugin.Instance.LoggerInstance.LogWarning("IsShotDeflectedByHeavyArmor");
-			return false;
-		}
-	}
+        public static V GetField<T, V>(string fieldName)
+        {
+            return (V)AccessTools.Field(typeof(T), fieldName).GetValue(null);
+        }
+    }
 
 }
